dd39328736695044d684ce0128fba82f4c9c8326
Stefan Sicklinger <mail@sicklinger.com>
2016-12-18 18:29:38 +0000

    Changed some python code

--- RasPi/tests/testIntercom.py
+++ RasPi/tests/testIntercom.py
@@ -1,9 +1,9 @@
 import serial
-ser = serial.Serial(‘/dev/ttyAMA0’, 9600, timeout=1)
-ser.open()
+ser = serial.Serial('/dev/ttyACM0', 9600)
+#ser.open()
 try:
-	while 1:
-		response = ser.readline()
-		print response
-		except KeyboardInterrupt:
-ser.close()
+  while 1:
+    response = ser.readline()
+    print response
+except KeyboardInterrupt:
+    ser.close()
