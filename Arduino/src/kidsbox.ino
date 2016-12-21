755e87d59500931834349f512ad5ebcdc53a3af9
Stefan Sicklinger <mail@sicklinger.com>
2016-12-21 19:28:51 +0000

    Added lib for mplayer

--- Arduino/src/kidsbox.ino
+++ Arduino/src/kidsbox.ino
@@ -1,15 +1,48 @@
+//Global variables
+int ledPinR = 9;  
+int ledPinG = 18;   
+int inputWhite=4;
+unsigned char buffer[64];//UART buffer
+int count = 0; 
 
-#define LED_PIN 13
-
-void setup()
-{
-    pinMode(LED_PIN, OUTPUT);
+void setup() {
+Serial.begin(9600);//Connect to RasPi
+Serial1.begin(9600);//Connect to RFID
+pinMode(ledPinR, OUTPUT);
+pinMode(ledPinG, OUTPUT);
+pinMode(inputWhite, INPUT_PULLUP); 
 }
 
-void loop()
+void loop() {
+ // Get data from RFID 
+  if (Serial1.available())              
+  {
+    while(Serial1.available())               // reading data into char array
+    {
+      buffer[count++] = Serial1.read();      // writing data into array
+      if(count == 64)break;
+     }
+     Serial.write(buffer, count);     // if no data transmission ends, write buffer to hardware serial port
+     clearBufferArray();             // call clearBufferArray function to clear the stored data from the array
+     count = 0;                      // set counter of while loop to zero
+  }
+// IO stuff 
+  if(digitalRead(inputWhite)==LOW){
+    digitalWrite(ledPinG, HIGH);
+    digitalWrite(ledPinR, LOW);
+  }
+  else
+  {
+    digitalWrite(ledPinG, LOW);
+    digitalWrite(ledPinR, HIGH);
+  }
+}
+
+void clearBufferArray()                 // function to clear buffer array
 {
-    digitalWrite(LED_PIN, HIGH);
-    delay(100);
-    digitalWrite(LED_PIN, LOW);
-    delay(900);
+    // clear all index of array with command NULL
+    for (int i=0; i<count; i++)
+    {
+        buffer[i]=0;
+    }                  
 }
