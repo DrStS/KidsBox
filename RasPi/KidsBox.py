3b483331eef3b706f25bdd9bcc84f4110e40a1ba
Stefan Sicklinger <mail@sicklinger.com>
2020-01-12 10:07:40 +0000

    python 3

--- RasPi/KidsBox.py
+++ RasPi/KidsBox.py
@@ -8,14 +8,14 @@ import os
 from random import randint
 #Get my files
 myMusicFolder= '/home/pi/music/'
-myMusicFiles = filter(lambda x: x.endswith('.mp3'), os.listdir(myMusicFolder))
+myMusicFiles = list(filter(lambda x: x.endswith('.mp3'), os.listdir(myMusicFolder)))
 myNumMusicFiles = len(myMusicFiles)-1
 
 kidsBoxPlayer = pymplb.MPlayer()
 kidsBoxPlayer.loadfile(myMusicFolder+myMusicFiles[randint(0,myNumMusicFiles)])
 #kidsBoxPlayer.loadlist('http://www.rockantenne.de/webradio/rockantenne.m3u')
 ser = serial.Serial('/dev/ttyACM0', 9600)
-ser.write('s')
+ser.write(str.encode('s'))
 kidsBoxMixer = alsaaudio.Mixer()
 kidsBoxVolume = 48
 kidsBoxVolumeMax = 62
@@ -23,46 +23,46 @@ kidsBoxMixer.setvolume(kidsBoxVolume)
 print("== KidsBox ready! ==")
 
 try:
-	while 1:
-		pauseByte=ser.read(1)
-		if pauseByte is 'r':
-			print("Resume")
-			kidsBoxPlayer.set_property('pause','no')
-		if pauseByte is 'p':		
-			print("Pause")
-			kidsBoxPlayer.set_property('pause','yes')
-                if pauseByte is 'u':
-                        print("Increase volume")
-                        kidsBoxVolume = int(kidsBoxVolume + 2)
-                        if kidsBoxVolume < kidsBoxVolumeMax:
-				print(kidsBoxVolume)
-				kidsBoxMixer.setvolume(kidsBoxVolume)
-			else:
-				kidsBoxVolume = kidsBoxVolumeMax
-                if pauseByte is 'd':
-                        print("Decrease volume")    
-                        kidsBoxVolume = int(kidsBoxVolume - 2)
-                        if kidsBoxVolume > 0:
-                                kidsBoxMixer.setvolume(kidsBoxVolume)
-			else:
-				kidsBoxVolume = 0
-                if pauseByte is 'f':
-                        print("Forward")
-                        fileNameTmp=myMusicFolder+myMusicFiles[randint(0,myNumMusicFiles)]
-                        print(fileNameTmp)
-                        kidsBoxPlayer.loadfile(fileNameTmp)
-                if pauseByte is 'b':
-                        print("Backward")
-                        kidsBoxPlayer.loadfile(myMusicFolder+myMusicFiles[randint(0,myNumMusicFiles)])
-                if pauseByte is 'q':
-                        print("RFID read")
-			RFIDvalue=ser.read(6)
-			print(RFIDvalue)
-			if str(RFIDvalue) == '193D74':
-				print("RFID CARD 1")
-				kidsBoxPlayer.loadlist('http://www.rockantenne.de/webradio/rockantenne.m3u')
-                        if str(RFIDvalue) == '1CC9DE':
-                                print("RFID CARD 2")
-                                kidsBoxPlayer.loadlist('http://www.rockantenne.de/webradio/rockantenne.m3u')
+    while 1:
+        pauseByte=ser.read(1)
+        if pauseByte is 'r':
+            print("Resume")
+            kidsBoxPlayer.set_property('pause','no')
+        if pauseByte is 'p':		
+            print("Pause")
+            kidsBoxPlayer.set_property('pause','yes')
+        if pauseByte is 'u':
+            print("Increase volume")
+            kidsBoxVolume = int(kidsBoxVolume + 2)
+            if kidsBoxVolume < kidsBoxVolumeMax:
+                print(kidsBoxVolume)
+                kidsBoxMixer.setvolume(kidsBoxVolume)
+            else:
+                kidsBoxVolume = kidsBoxVolumeMax
+        if pauseByte is 'd':
+            print("Decrease volume")    
+            kidsBoxVolume = int(kidsBoxVolume - 2)
+            if kidsBoxVolume > 0:
+                kidsBoxMixer.setvolume(kidsBoxVolume)
+            else:
+                kidsBoxVolume = 0
+        if pauseByte is 'f':
+            print("Forward")
+            fileNameTmp=myMusicFolder+myMusicFiles[randint(0,myNumMusicFiles)]
+            print(fileNameTmp)
+            kidsBoxPlayer.loadfile(fileNameTmp)
+        if pauseByte is 'b':
+            print("Backward")
+            kidsBoxPlayer.loadfile(myMusicFolder+myMusicFiles[randint(0,myNumMusicFiles)])
+        if pauseByte is 'q':
+            print("RFID read")
+            RFIDvalue=ser.read(6)
+            print(RFIDvalue)
+            if str(RFIDvalue) == '193D74':
+                print("RFID CARD 1")
+                kidsBoxPlayer.loadlist('http://www.rockantenne.de/webradio/rockantenne.m3u')
+            if str(RFIDvalue) == '1CC9DE':
+                print("RFID CARD 2")
+                kidsBoxPlayer.quit()				
 except KeyboardInterrupt:
 	ser.close()
